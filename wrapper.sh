b3ebe671118e1c76eaa291503455fa163dddaeec
Don MacMillen <don@macmillen.net>
2023-11-29 20:17:06 -0800

    define NDEBUG to remove 18K assert's (only in libabc.so)
    Signed-off-by: Don MacMillen <don@macmillen.net>

--- wrapper.sh
+++ wrapper.sh
@@ -4,19 +4,20 @@
 # that yet)
 # Finally, you must have write access to site-packages to do the pip install -e ...
 make clean
-make OPTFLAGS=-O ABC_USE_PIC=1 ABC_USE_NO_READLINE=1 libabc.so -j8
+# define NDEBUG so that all the 18K asserts are no-ops (lots of unused variables remain)
+CFLAGS=-DNDEBUG make OPTFLAGS=-O ABC_USE_PIC=1 ABC_USE_NO_READLINE=1 libabc.so -j8
 cd ABC-python
 swig -python ABC.i
 hdrs=(`locate Python.h`)
 VAR=${hdrs[-1]}
 DIR=${VAR%/*}
 # If locate doesn't work, try something like the following
-#DIR=/home/macd/anaconda3/include/python3.8
+#DIR=/home/macd/anaconda3/include/python3.11
 gcc -fPIC -I$DIR -I../src ABC_wrap.c -c
 g++ -shared -fPIC -o _ABC.so `find .. -name \*.o`
 cd ..
 pip install -e ABC-python
 
-# now make the executable abc with readline enabled.
+# now make the executable abc with readline and assert enabled.
 make clean
 make OPTFLAGS=-O -j8
